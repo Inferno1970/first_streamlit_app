33dd79530bac4889b9559b1246e5809a9b4ba11a
Sepehr Nem <69166116+Inferno1970@users.noreply.github.com>
2023-02-15 12:42:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 #created the main python  file
 import streamlit
-streamlit.title('My parent new healthy diner')
+streamlit.title('My Parent New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Range Egg')
